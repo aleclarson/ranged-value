586a5d779228f58e59f4e0466867052db6ef6971
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-02-21 01:43:00 -0800

    2/20/2016

--- js/src/RangedValue.js
+++ js/src/RangedValue.js
@@ -2,7 +2,7 @@ var Factory, Interpolation, Listenable, NativeValue, Reaction, ReactiveGetter, e
 
 NativeValue = require("component").NativeValue;
 
-ReactiveGetter = require("reactive-getter");
+ReactiveGetter = require("ReactiveGetter");
 
 Interpolation = require("Interpolation");
 
@@ -20,24 +20,37 @@ module.exports = Factory("RangedValue", {
     return {};
   },
   optionTypes: {
-    get: Function,
+    value: [NativeValue, Number, Void],
+    reaction: [Reaction, Void],
+    get: [Function, Void],
+    didSet: [Function, Void],
     range: Array,
-    dampen: Array,
-    didSet: Function
+    dampen: [Array, Void]
   },
   optionDefaults: {
     dampen: [0, 0],
     didSet: emptyFunction
   },
   customValues: {
+    value: {
+      get: function() {
+        return this._reaction.value;
+      }
+    },
     minValue: {
       get: function() {
-        return this._range[0];
+        return this.range[0];
+      },
+      set: function(minValue) {
+        return this.range[0] = minValue;
       }
     },
     maxValue: {
       get: function() {
-        return this._range[1];
+        return this.range[1];
+      },
+      set: function(maxValue) {
+        return this.range[1] = maxValue;
       }
     },
     getValue: {
@@ -48,35 +61,60 @@ module.exports = Factory("RangedValue", {
           };
         })(this);
       }
+    },
+    dampen: {
+      value: null,
+      didSet: function(dampen) {
+        return this._dampen = this._createDampener();
+      }
     }
   },
-  initFrozenValues: function(options) {
+  initValues: function(options) {
     return {
-      _get: ReactiveGetter(options.get),
+      range: options.range,
+      reaction: options.reaction,
+      nativeValue: options.value != null ? NativeValue(options.value) : void 0,
+      _get: options.get,
       _didSet: options.didSet,
-      _range: options.range,
-      _dampen: this._createDampener(options.dampen)
+      _reaction: null,
+      _dampen: null
     };
   },
-  initReactiveValues: function(options) {
-    return {
-      value: Reaction.sync({
-        get: (function(_this) {
-          return function() {
-            return _this._dampen(_this._get());
-          };
-        })(this),
-        didSet: (function(_this) {
-          return function(newValue) {
-            _this._didSet(newValue);
-            return _this._emit("didSet", newValue);
-          };
-        })(this)
-      })
-    };
-  },
-  init: function() {
-    return Listenable(this);
+  init: function(options) {
+    Listenable(this, {
+      eventNames: true
+    });
+    this.dampen = options.dampen;
+    if (this._get != null) {
+
+    } else if (this.nativeValue != null) {
+      this._get = this.nativeValue.getValue;
+    } else if (this.reaction != null) {
+      this._get = (function(_this) {
+        return function() {
+          return _this.reaction.value;
+        };
+      })(this);
+    }
+    return this._reaction = Reaction.sync({
+      get: (function(_this) {
+        return function() {
+          var value;
+          value = _this._get();
+          if (value != null) {
+            return _this._dampen(value);
+          } else {
+            return null;
+          }
+        };
+      })(this),
+      didSet: (function(_this) {
+        return function(newValue, oldValue) {
+          _this._didSet(newValue, oldValue);
+          return _this._emit("didSet", newValue);
+        };
+      })(this)
+    });
   },
   getProgress: function(arg) {
     var clamp, progress;
@@ -88,10 +126,12 @@ module.exports = Factory("RangedValue", {
     return Math.max(0, Math.min(1, progress));
   },
   detach: function() {},
-  _createDampener: function(dampenValues) {
+  _createDampener: function() {
+    var max, min, ref;
+    ref = this.dampen, min = ref[0], max = ref[1];
     return Interpolation.create({
-      inputRange: [this.minOffset - 10, this.minOffset, this.maxOffset, this.maxOffset + 10],
-      outputRange: [this.minOffset - (dampenValues[0] === 0 ? 0 : Math.pow(10, dampenValues[0])), this.minOffset, this.maxOffset, this.maxOffset + (dampenValues[1] === 0 ? 0 : Math.pow(10, dampenValues[1]))]
+      inputRange: [this.minValue - 10, this.minValue, this.maxValue, this.maxValue + 10],
+      outputRange: [this.minValue - (min === 0 ? 0 : Math.pow(10, min)), this.minValue, this.maxValue, this.maxValue + (max === 0 ? 0 : Math.pow(10, max))]
     });
   }
 });
--- src/RangedValue.coffee
+++ src/RangedValue.coffee
@@ -1,7 +1,7 @@
 
 { NativeValue } = require "component"
 
-ReactiveGetter = require "reactive-getter"
+ReactiveGetter = require "ReactiveGetter"
 Interpolation = require "Interpolation"
 emptyFunction = require "emptyFunction"
 Listenable = require "listenable"
@@ -15,10 +15,12 @@ module.exports = Factory "RangedValue",
   create: -> {} # Pretending to inherit from NativeValue.
 
   optionTypes:
-    get: Function
+    value: [ NativeValue, Number, Void ]
+    reaction: [ Reaction, Void ]
+    get: [ Function, Void ]
+    didSet: [ Function, Void ]
     range: Array
-    dampen: Array
-    didSet: Function
+    dampen: [ Array, Void ]
 
   optionDefaults:
     dampen: [ 0, 0 ]
@@ -26,55 +28,84 @@ module.exports = Factory "RangedValue",
 
   customValues:
 
-    minValue: get: ->
-      @_range[0]
+    value: get: ->
+      @_reaction.value
 
-    maxValue: get: ->
-      @_range[1]
+    minValue:
+      get: -> @range[0]
+      set: (minValue) ->
+        @range[0] = minValue
 
+    maxValue:
+      get: -> @range[1]
+      set: (maxValue) ->
+        @range[1] = maxValue
+
+    # Matches the NativeValue API.
     getValue: lazy: ->
       return => @value
 
-  initFrozenValues: (options) ->
-    _get: ReactiveGetter options.get
+    dampen:
+      value: null
+      didSet: (dampen) ->
+        @_dampen = @_createDampener()
+
+  initValues: (options) ->
+    range: options.range
+    reaction: options.reaction
+    nativeValue: NativeValue options.value if options.value?
+    _get: options.get
     _didSet: options.didSet
-    _range: options.range
-    _dampen: @_createDampener options.dampen
+    _reaction: null
+    _dampen: null
 
-  initReactiveValues: (options) ->
+  init: (options) ->
 
-    value: Reaction.sync
-      get: => @_dampen @_get()
-      didSet: (newValue) =>
-        @_didSet newValue
+    Listenable this, { eventNames: yes }
+
+    @dampen = options.dampen
+
+    if @_get?
+      # no-op
+    else if @nativeValue?
+      @_get = @nativeValue.getValue
+    else if @reaction?
+      @_get = => @reaction.value
+
+    @_reaction = Reaction.sync
+      get: =>
+        value = @_get()
+        if value? then @_dampen value
+        else null
+      didSet: (newValue, oldValue) =>
+        @_didSet newValue, oldValue
         @_emit "didSet", newValue
-
-  init: ->
-
-    Listenable this
 
   getProgress: ({ clamp } = {}) ->
     progress = (@value - @minValue) / (@maxValue - @minValue)
     return progress unless clamp
     Math.max 0, Math.min 1, progress
 
+  # Matches the NativeValue API.
   detach: ->
     # TODO Remove reaction?
 
-  _createDampener: (dampenValues) ->
+  _createDampener: ->
+
+    [ min, max] = @dampen
 
     return Interpolation.create
 
       inputRange: [
-        @minOffset - 10
-        @minOffset
-        @maxOffset
-        @maxOffset + 10
+        @minValue - 10
+        @minValue
+        @maxValue
+        @maxValue + 10
       ]
 
       outputRange: [
-        @minOffset - if dampenValues[0] is 0 then 0 else Math.pow 10, dampenValues[0]
-        @minOffset
-        @maxOffset
-        @maxOffset + if dampenValues[1] is 0 then 0 else Math.pow 10, dampenValues[1]
+        @minValue - if min is 0 then 0 else Math.pow 10, min
+        @minValue
+        @maxValue
+        @maxValue + if max is 0 then 0 else Math.pow 10, max
       ]
